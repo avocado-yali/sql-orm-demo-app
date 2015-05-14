cdea92800310c8d8550e6e8449a71e7c3347e96b
vitrum <vitrum.cn@gmail.com>
2015-05-14 13:35:45 +0800

    add black project

---
+++ addplugin.sh
@@ -0,0 +1,9 @@
+
+ionic plugin add com.ionic.keyboard
+
+
+ionic plugin add io.litehelpers.cordova.sqlite
+
+# ionic plugin add https://github.com/brodysoft/Cordova-SQLitePlugin.git
+
+ionic plugin add org.apache.cordova.splashscreen
